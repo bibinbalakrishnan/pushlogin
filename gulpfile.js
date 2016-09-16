f480db4aa037d8fa22eb3b319dde83eb9f1938b0
bibinbalakrishnan <bibin.balakrishnan@gmail.com>
2016-09-16 20:26:58 +0800

    updated gitignore

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,10 @@
 var gulp=require('gulp'),nodemon=require('gulp-nodemon');
 
-
+//serve task
 gulp.task('serve', function () {
   nodemon({
     script: 'server.js'
   , ext: 'js html'
-  
   , env: { 'NODE_ENV': 'development' }
   })
 })
